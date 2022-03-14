a73f54f90c4d3de8a77be062f58c3d4bde47d695
Arushi Somani <arushisomani@berkeley.edu>
2022-03-14 11:34:27 -0700

    updating app.js

--- app.js
+++ app.js
@@ -4,19 +4,22 @@ var http = require('http');
 var app = express();
 var server = http.createServer(app);
 
-// REMOVE FOR DEMO
 var io = require('socket.io')(server);
+var path = require('path');
+
 
 app.use(express.static(path.join(__dirname,'./public')));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
+
 
 // REMOVE FOR DEMO
 io.on('connection', (socket) => {
     socket.on('signup', (name) => {
         socket.broadcast.emit('signup', name);
-    })
+    });
 });
 
 server.listen(process.env.PORT || 3000, () => {
